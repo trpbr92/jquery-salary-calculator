7fe49a3775f8b941d5da17b10ddf89356f27cdf9
Trace Brandt <trpbr92@gmail.com>
2021-03-05 16:19:23 -0600

    clear text fields

--- scripts/client.js
+++ scripts/client.js
@@ -1,5 +1,28 @@
 $ (document).ready(onReady)
 
+let employeeInfo = [];
+
 function onReady(){
     console.log('JQ');
+ $('#submitButton').on('click', getInfo)   
 }//end onready
+
+function getInfo(){
+  console.log('Clicked!'); 
+  const firstName = $('#firstNameInput').val();
+  const lastName = $('#lastNameInput').val();
+  employeeObject = {
+    firstName: firstName,
+    lastName: lastName
+  }//endEmployeeObject
+  
+  employeeInfo
+  el = $()
+
+$('#firstNameInput').val('');
+$('#lastNameInput').val('');
+$('#idInput').val('');
+$('#titleInput').val('');
+$('#salaryInput').val('');
+}//end getInfo
+
